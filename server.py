4b3cea76273fc8d75c691b581ef4edf689ef21ec
ezhillragesh <rag3shx@gmail.com>
2024-02-25 17:40:05 +0530

    fixed cors error

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from Termsfinder import websScrape
 
 app = Flask(__name__)
+CORS(app, resources={r"/darkpattern": {"origins": "*"}})  # Allow requests to the '/darkpattern' endpoint from all origins
 
 @app.route('/darkpattern', methods=['POST'])
 def post_endpoint():
